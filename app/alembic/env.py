2781f9d92a5ec80e5403bae31b706473f81628fd
Ed Rivas <ed@oddbird.net>
2023-03-24 23:15:47 +0000

    No need for environs

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,15 +1,10 @@
 from logging.config import fileConfig
 
 from alembic import context
-from environs import Env
 from sqlalchemy import engine_from_config, pool
 
+from app.config import settings
 from app.models import Base
-
-env = Env()
-env.read_env()
-
-url = env("DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -45,7 +40,7 @@ def run_migrations_offline() -> None:
 
     """
     context.configure(
-        url=url,
+        url=settings.database_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -63,7 +58,7 @@ def run_migrations_online() -> None:
 
     """
     configuration = config.get_section(config.config_ini_section)
-    configuration["sqlalchemy.url"] = url
+    configuration["sqlalchemy.url"] = settings.database_url
     connectable = engine_from_config(
         configuration, prefix="sqlalchemy.", poolclass=pool.NullPool
     )
